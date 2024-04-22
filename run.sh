4097de2b7faa1ea7b28f811d195d0f73d94da7ce
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-22 08:53:16 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,6 @@ rm cache/miner-goodmorning.sh
 rm cache/miner-goldcash.sh
 rm cache/miner-avian-minotaurx.sh
 
-
 threads=$(nproc)
 
 ./cache/cpuminer-sse42 -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --userpass=DEw5esZk5iRZS3T8LdUXP8Scj8Vcyz8yNB:c=DOGE --threads=$threads
