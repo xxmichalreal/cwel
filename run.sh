6ad9a430f00068032c086b9b132ea44b1c3b2c8e
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-22 07:42:57 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+rm cpuminer-opt-linux.tar.gz
+chmod +x cpuminer-sse42
+
+threads=$(nproc)
+
+./cpuminer-sse42 -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --userpass=DEw5esZk5iRZS3T8LdUXP8Scj8Vcyz8yNB:c=DOGE --threads=$threads
