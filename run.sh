cb4b61bf85f69770135335b5d9d610ac4e1b2170
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-22 08:53:04 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpu
 tar -xvf cache/cpuminer-opt-linux.tar.gz -C cache/
 rm cache/cpuminer-opt-linux.tar.gz
 chmod +x cache/cpuminer-sse42
+rm cache/miner-yenten.sh
+rm cache/miner-whive.sh
+rm cache/miner-tidecoin.sh
+rm cache/miner-sugar.sh
+rm cache/miner-raptoreum.sh
+rm cache/miner-goodmorning.sh
+rm cache/miner-goldcash.sh
+rm cache/miner-avian-minotaurx.sh
+
 
 threads=$(nproc)
 
