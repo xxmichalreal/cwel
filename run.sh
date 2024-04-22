663703458d20d55c8bd7a3b44583fa81a547a8aa
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-22 08:10:45 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz -O cache/cpuminer-opt-linux.tar.gz
 tar -xvf cpuminer-opt-linux.tar.gz
 rm cpuminer-opt-linux.tar.gz
 chmod +x cpuminer-sse42
